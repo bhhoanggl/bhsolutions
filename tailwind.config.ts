b3444c64e8bf196de1ab882846b4d855998ced41
bhhoang818 <42143903+bhhoanggl@users.noreply.github.com>
2025-10-25 10:12:02 +0700

    add

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { heroui } from "@heroui/theme";
+
+const config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
+export default config;
